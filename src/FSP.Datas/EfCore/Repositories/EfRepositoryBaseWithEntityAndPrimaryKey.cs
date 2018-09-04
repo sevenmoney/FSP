cab92b41a66506ebec61621c3a88ed04f7b4597e
sevenmoney <qiu756863752@163.com>
2018-09-04 22:28:05 +0800

    buid

--- src/FSP.Datas/EfCore/Repositories/EfRepositoryBaseWithEntityAndPrimaryKey.cs
+++ src/FSP.Datas/EfCore/Repositories/EfRepositoryBaseWithEntityAndPrimaryKey.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using FSP.Core.Collections.Extensions;
 using FSP.Core.Domain.Entities;
 using FSP.Core.Domain.Repositories;
@@ -46,20 +48,67 @@ namespace FSP.Datas.EfCore.Repositories
         }
 
         public override TEntity Update(TEntity entity) {
+            AttachIfNot(entity);
             Context.Entry(entity).State = EntityState.Modified;
             return entity;
         }
+        public override Task<TEntity> UpdateAsync(TEntity entity) {
+            AttachIfNot(entity);
+            Context.Entry(entity).State = EntityState.Modified;
+            return Task.FromResult(entity);
+        }
 
         public override void Delete(TEntity entity) {
+            AttachIfNot(entity);
             Table.Remove(entity);
         }
 
         public override void Delete(TPrimaryKey id) {
-            throw new System.NotImplementedException();
+            var entity = GetFromChangeTrackerOrNull(id);
+            if(entity != null) {
+                Delete(entity);
+                return;
+            }
+
+            entity = FirstOrDefault(id);
+            if(entity != null) {
+                Delete(entity);
+                return;
+            }
         }
 
         public DbContext GetDbContext() {
-            throw new System.NotImplementedException();
+            return Context;
+        }
+
+        /// <summary>
+        /// 实体不存在则附加到数据表中
+        /// </summary>
+        /// <param name="entity"></param>
+        protected virtual void AttachIfNot(TEntity entity) {
+            var entry = Context.ChangeTracker.Entries().FirstOrDefault(ent => ent.Entity == entity);
+            if(entry != null) {
+                return;
+            }
+
+            Table.Attach(entity);
+        }
+
+        /// <summary>
+        /// 根据主键Id获取追踪列表中的实体
+        /// 没有则返回null
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        private TEntity GetFromChangeTrackerOrNull(TPrimaryKey id) {
+            var entry = Context.ChangeTracker.Entries()
+                .FirstOrDefault(
+                    ent =>
+                        ent.Entity is TEntity &&
+                        EqualityComparer<TPrimaryKey>.Default.Equals(id, (ent.Entity as TEntity).Id)
+                );
+
+            return entry?.Entity as TEntity;
         }
     }
 }
