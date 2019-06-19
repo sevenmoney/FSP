7a964c9f580c5894e6606a7c6ed3b825126fb537
sevenmoney <qiu756863752@163.com>
2019-06-19 21:41:05 +0800

    migration

--- sample/Data/Migrations/20190619131547_20190619.Designer.cs
+++ sample/Data/Migrations/20190619131547_20190619.Designer.cs
@@ -10,14 +10,14 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(SampleDbContext))]
-    [Migration("20190618012245_20190618")]
-    partial class _20190618
+    [Migration("20190619131547_20190619")]
+    partial class _20190619
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.8-servicing-32085")
+                .HasAnnotation("ProductVersion", "2.1.11-servicing-32099")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
--- sample/Data/Migrations/20190619131547_20190619.cs
+++ sample/Data/Migrations/20190619131547_20190619.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Data.Migrations
 {
-    public partial class _20190618 : Migration
+    public partial class _20190619 : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -18,12 +18,28 @@ namespace Data.Migrations
                 {
                     table.PrimaryKey("PK_Role", x => x.Id);
                 });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserName = table.Column<string>(maxLength: 20, nullable: true),
+                    UserAddress = table.Column<string>(maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
                 name: "Role");
+
+            migrationBuilder.DropTable(
+                name: "User");
         }
     }
 }
